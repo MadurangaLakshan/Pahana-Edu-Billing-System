da3606f761368542101ff8f0e8571eb1a496442e
MadurangaLakshan <madurangalakshan600@gmail.com>
2025-08-18 15:43:50 +0530

    Initial commit

---
+++ pahana-edu-billing-system/src/main/java/com/pahana/model/Admin.java
@@ -0,0 +1,36 @@
+package com.pahana.model;
+
+
+public class Admin extends User{
+	
+
+	public Admin(int userId, String username, String password, String role) {
+		super(userId, username, password, role);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void manageUsers( ) {
+		
+	}
+	
+	public void manageItems() {
+		
+	}
+	
+	public void manageCustomers() {
+		
+	}
+	
+	public void generateReport() {
+		
+	}
+
+	public void viewAllBills() {
+		
+	}
+	
+	public void viewHelpSection() {
+		
+	}
+}
+
---
+++ pahana-edu-billing-system/src/main/java/com/pahana/model/Staff.java
@@ -0,0 +1,29 @@
+package com.pahana.model;
+
+public class Staff extends User{
+
+	public Staff(int userId, String username, String password, String role) {
+		super(userId, username, password, role);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void manageCustomers () {
+		
+	}
+	
+	public void searchCustomer() {
+		
+	}
+	
+	public void calculateBill() {
+		
+	}
+	
+	public void printBill() {
+		
+	}
+	
+	public void viewHelpSection() {
+		
+	}
+}
